7e8ad756cfaae499f8784e4c83464f1d29b42420
Pipa Stanislav <pipa.staninsav@gmail.com>
2017-12-02 00:14:47 +0200

    Task_6
    Two variants for solving the task six

---
+++ Task_6/Task_6.v1.js
@@ -0,0 +1,18 @@
+var Push = function foo (){
+	console.log(arr);
+	var arr = [1,2,-3,-4,5,6,-7,-8,9,10];
+	var result = [];
+	for (var j = 0; j < arr.length; j++ ){
+		if (arr [j] < 0){
+			result.push(arr[j])
+		}
+	}
+	document.write ( "The first variant of the solution"
+		,"<br>" + arr
+		,"<br>" + "Result " + result
+		,'<br>'
+		,'<br>'
+		);
+	;
+	return [arr, result];
+}
